bb8564e12ebbebc5abc2fb455bf2000ee5fbaeca
radwija <115165737+radwija@users.noreply.github.com>
2023-06-17 21:56:36 +0800

    first init

---
+++ src/main/java/com/learn/demo/LearnGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.learn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnGitDemoApplication.class, args);
+	}
+
+}
